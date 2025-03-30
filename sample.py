86bb818ec509a4a9198dd404d122c941d30563a6
TelloCS <jeremytello31@gmail.com>
2025-03-30 13:42:43 -0500

    new subtraction function

--- sample.py
+++ sample.py
@@ -2,6 +2,9 @@ def add(num1, num2) -> int:
     result = num1 + num2
     return result
 
+def subtraction(num1, num2) -> int:
+    result = num1 - num2
+    return result
 
 if __name__ == "__main__":
-    print(add(2, 4))
+    print(subtraction(4, 0))
